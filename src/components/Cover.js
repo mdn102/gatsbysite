3bbe2f62ccf507be5f83828e60cb228777526d18
nguyendm77 <nguyendm77@gmail.com>
2021-02-02 13:02:16 -0800

    modify resume link

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -33,7 +33,7 @@ class Cover extends React.Component {
           <source src="https://res.cloudinary.com/djhte2ard/video/upload/v1599667813/dev_background_purctc.mp4" type="video/mp4" />
         </video>
         <div className={'dashboard-down-arrow-container'}>
-          <a href="./resume.pdf" target="_blank" rel="no-opener no-referrer" className={'resume-container'}><span>HIRING ME TODAY</span></a>
+          <a href="resume.pdf" target="_blank" rel="no-opener no-referrer" className={'resume-container'}><span>HIRING ME TODAY</span></a>
         </div>
       </div>
     );
